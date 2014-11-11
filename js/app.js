34a745730218dbbfb3c40f46cf4a2561f69306d0
Ed Hubbell <edhubbell@gmail.com>
2014-11-10 21:51:14 -0500

    getting rid of .map in js

--- js/app.js
+++ js/app.js
@@ -81,46 +81,6 @@ ngApp.directive('sizeColumn', function($timeout) {
 });
 
 
-ngapp.directive('fbLike', [
-      '$window', '$rootScope', function($window, $rootScope) {
-        return {
-            restrict: 'A',
-            scope: {
-                fbLike: '=?'
-            },
-            link: function (scope, element, attrs) {
-                if (!$window.FB) {
-                    // Load Facebook SDK if not already loaded
-                    $.getScript('//connect.facebook.net/en_US/sdk.js', function () {
-                        $window.FB.init({
-                            appId: '728778103878454',
-                            xfbml: true,
-                            version: 'v2.1'
-                        });
-                        renderLikeButton();
-                    });
-                } else {
-                    renderLikeButton();
-                }
- 
-                function renderLikeButton() {
-                    if (!!attrs.fbLike && !scope.fbLike) {
-                        // wait for data if it hasn't loaded yet
-                        scope.$watch('fbLike', function () {
-                            renderLikeButton();
-                        });
-                        return;
-                    } else {
-                        element.html('<div class="fb-like"' + (!!scope.fbLike ? ' data-href="' + scope.fbLike + '"' : '') + ' data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>');
-                        $window.FB.XFBML.parse(element.parent()[0]);
-                    }
-                }
-            }
-        };
-      }
-  ]);
-
-
 function reset_itemHeight() {
   	$('.items').each(function(){  
           var highestBox = 0;
