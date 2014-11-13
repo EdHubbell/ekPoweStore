9c4cd2d52bf4f06266cd956b0b5bd35e20ab8abd
Ed Hubbell <edhubbell@gmail.com>
2014-11-12 23:00:13 -0500

    one more and we're all set.  much easier way to solve this problem

--- js/app.js
+++ js/app.js
@@ -30,6 +30,10 @@ ngApp.controller('main', function($scope, $http, $timeout){
 	        simpleSheet: true
 	  })
 
+    var saleEndDate = new Date(2014, 10, 21, 18, 30, 0, 0);
+    console.log(saleEndDate);           
+
+
  
 	function createStore (data,tabletop) {
     
